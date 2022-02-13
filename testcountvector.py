98f8625e83318ebb3e7375bf4599ca93aba5aa9c
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-13 18:22:11 +0500

    Add files via upload

---
+++ testcountvector.py
@@ -0,0 +1,31 @@
+from sentences import Sentences
+from words import Words
+from punctuation import Punctuation
+import corpus
+import countvector
+
+llist = []
+rlist = []
+nlist = []
+ylist = []
+with open("data.txt", "r") as file:
+    for line in file:
+        if not line:
+            continue
+        else:
+            left, right, *res = line.split(":")
+            llist.append(left)
+            rlist.append(right)
+
+"""
+for i in llist:
+    insth = Words(i)
+    w = insth.load()
+    nlist.append(w)
+for i in nlist:
+    stop = corpus.stopwords("russian")
+    stopfiltered = [str(x) for x in i if x not in stop]
+    ylist.append(stopfiltered)
+"""
+vect = countvector.vector(llist)
+print(vect)
