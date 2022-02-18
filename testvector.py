5f953fa743a4047b5a53b0e840162a0d0f7f5c13
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-19 04:53:10 +0500

    Add files via upload

---
+++ testvector.py
@@ -0,0 +1,37 @@
+from sentences import Sentences
+from words import Words
+from punctuation import Punctuation
+from lemmatize import Lemmatize
+import corpus
+import vector
+
+xlist = []
+
+# to awoid some problems with not good writting text
+# для избежания возможной ошибки при неправильном форматировании данных
+with open("data.txt", "r") as file:
+    for row in file:
+        if not row:
+            continue
+        else:
+            xlist.append(row)
+        varstring = " ".join(xlist)
+
+# tokenise to sentences
+s = Sentences(varstring)
+sl = s.load()
+print("Sentences\n", sl)
+
+# vector with tf-idf
+vect = vector.vector(sl, tfidf = "tfidf")
+
+# vectors words
+vectwords = vector.linetokenize()
+print("vectors words\n", vectwords)
+
+# max
+for i in vect:
+    for j in i:
+        if j == max(i):
+            print("out: ", j)
+
