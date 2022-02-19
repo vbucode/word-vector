152ef63ab6903715b344f2c2a77ecc411eaf8f87
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-19 23:49:30 +0500

    Add files via upload

--- testvector.py
+++ testvector.py
@@ -6,37 +6,43 @@ import corpus
 import vector
 
 xlist = []
+outdict = {}
 
 # to awoid some problems with not good writting text
 # для избежания возможной ошибки при неправильном форматировании данных
-with open("data.txt", "r") as file:
-    for row in file:
-        if not row:
+with open("fooddeliverydatabaseclean.txt", "r") as file:
+    for line in file:
+        if not line:
             continue
         else:
-            xlist.append(row)
-        varstring = " ".join(xlist)
+            xlist.append(line)
+        varstring = " . ".join(xlist)
 
-# tokenise to sentences
-s = Sentences(varstring)
+# tokenise to sentences(sometime need make tokenize sentences only dot)
+s = Sentences(varstring, dot = 1)
 sl = s.load()
-print("Sentences\n", sl)
+#print("Sentences\n", sl)
 
 # vector with tf-idf
 vect = vector.vector(sl, tfidf = "tfidf")
 
 # vectors words
 vectwords = vector.linetokenize()
-print("vectors words\n", vectwords)
+#print("vectors words\n", vectwords)
 
 # max
 for i in vect:
     for j in i:
         if j == max(i):
             try:
-                print("out: ", vectwords[vect.index(i)][i.index(j)], j)
+                outdict[vectwords[vect.index(i)][i.index(j)]] = j
             except IndexError:
                 pass
+        elif j == 0:
+            pass
         else:
-            continue
-
+            try:
+                outdict[vectwords[vect.index(i)][i.index(j)]] = j
+            except IndexError:
+                pass
+print(sorted(outdict.values()))
