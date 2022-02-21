513ff33378891b3200a2d62b1cf00e59bb6623ac
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-22 04:44:37 +0500

    Update testvector.py

--- testvector.py
+++ testvector.py
@@ -8,41 +8,36 @@ import vector
 xlist = []
 outdict = {}
 
-# to awoid some problems with not good writting text
-# для избежания возможной ошибки при неправильном форматировании данных
 with open("data.txt", "r") as file:
     for line in file:
         if not line:
             continue
         else:
             xlist.append(line)
-        varstring = " ".join(xlist)
+    varstring = " ".join(xlist)
 
 # tokenise to sentences(sometime need make tokenize sentences only dot)
 s = Sentences(varstring, dot = 1)
 sl = s.load()
-print("Sentences\n", sl)
 
 # vector with tf-idf
 vect = vector.vector(sl, tfidf = "tfidf")
 
 # vectors words
 vectwords = vector.linetokenize()
-print("vectors words\n", vectwords)
 
 for i in vect:
     for j in i:
-        if j == max(i):
+        if j != 0:
             try:
                 outdict[vectwords[vect.index(i)][i.index(j)]] = j
             except IndexError:
                 pass
-        elif j == 0:
-            pass
-        else:
-            try:
-                outdict[vectwords[vect.index(i)][i.index(j)]] = j
-            except IndexError:
-                pass
-
-print(outdict)
+sv = sorted(outdict.values())
+wsv = {}
+for i in sv:
+    for j in outdict.keys():
+        if outdict[j] == i:
+            wsv[j] = outdict[j]
+            break
+print(wsv)
