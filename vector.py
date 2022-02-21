300a055d9fec78c0038acd78439fb4695f9cf1d1
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-22 02:00:44 +0500

    Add files via upload

--- vector.py
+++ vector.py
@@ -25,46 +25,30 @@ def vector(getlist, tfidf = 0):
         insth = Words(i)
         w = insth.load()
         nlist.append(w)
-    if tfidf == "tf":
-        for i in range(len(nlist)):
+    for i in range(len(nlist)):
             klist.append([0]*len(instv))
-        for k in instv:
-            c = instv.count(k)
-            for i in nlist:
-                for j in i:
-                    if k == j:
-                        klist[nlist.index(i)][instv.index(k)] = c / len(instv)
-                        c = 0
+    if tfidf == "tf":
+        for i in nlist:
+            for j in i:
+                c = instv.count(j)
+                klist[nlist.index(i)][instv.index(j)] = c / len(instv)
 
     elif tfidf == "idf":
-        for i in range(len(nlist)):
-            klist.append([0]*len(instv))
-        for k in instv:
-            for i in nlist:
-                for j in i:
-                    if k == j:
-                        klist[nlist.index(i)][instv.index(k)] = math.log10(len(nlist)/sum([1.0 for i in nlist if k in i]))
+        for i in nlist:
+            for j in i:
+                klist[nlist.index(i)][instv.index(j)] = math.log10(len(nlist)/sum([1.0 for i in nlist if j in i]))
 
     elif tfidf == "tfidf":
-        for i in range(len(nlist)):
-            klist.append([0]*len(instv))
-        for k in instv:
-            c = instv.count(k)
-            for i in nlist:
-                for j in i:
-                    if k == j:
-                        vartf = c / len(instv)
-                        varidf = math.log10(len(nlist)/sum([1.0 for i in nlist if k in i]))
-                        klist[nlist.index(i)][instv.index(k)] = vartf * varidf
-                        c = 0
-        
+        for i in nlist:
+            for j in i:
+                c = instv.count(j)
+                vartf = c / len(instv)
+                varidf = math.log10(len(nlist)/sum([1.0 for i in nlist if j in i]))
+                klist[nlist.index(i)][instv.index(j)] = vartf * varidf
+
     elif tfidf == 0:
-        for i in range(len(nlist)):
-            klist.append([0]*len(instv))
-        for k in instv:
-            for i in nlist:
-                for j in i:
-                    if k == j:
-                        klist[nlist.index(i)][instv.index(k)] = 1
+        for i in nlist:
+            for j in i:
+                klist[nlist.index(i)][instv.index(j)] = 1
 
     return klist
