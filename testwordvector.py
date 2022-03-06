9867feb1e164b1575efbace111cc93b06e407689
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-03-06 14:03:17 +0500

    Add files via upload

---
+++ testwordvector.py
@@ -0,0 +1,53 @@
+from sentences import Sentences
+from words import Words
+from punctuation import Punctuation
+from lemmatize import Lemmatize
+import corpus
+import wordvector
+
+xlist = []
+outdict = {}
+
+with open("data.txt", "r") as file:
+    for line in file:
+        if not line:
+            continue
+        else:
+            xlist.append(line)
+        varstring = " ".join(xlist)
+"""
+with open("chatbotdatabase.txt", "r") as file:
+    for line in file:
+        if not line:
+            continue
+        else:
+            left, right, *res = line.split(":")
+            xlist.append(left)
+    #varstring = " ".join(xlist)
+"""
+# tokenise to sentences(sometime need make tokenize sentences only dot)
+s = Sentences(varstring, dot = 1)
+sl = s.load()
+
+# vector with tf-idf
+vect = wordvector.vector(xlist, tfidf = "tfidf")
+
+# vectors words
+vectwords = wordvector.linetokenize()
+#print("vectors words\n", vectwords)
+
+for i in vect:
+    for j in i:
+        if j != 0:
+            try:
+                outdict[vectwords[vect.index(i)][i.index(j)]] = j
+            except IndexError:
+                pass
+sv = sorted(outdict.values())
+wsv = {}
+for i in sv:
+    for j in outdict.keys():
+        if outdict[j] == i:
+            wsv[j] = outdict[j]
+            break
+print("tf-idf\n", wsv)
---
+++ wordvector.py
@@ -0,0 +1,54 @@
+# module to count vector
+import math
+from words import Words
+
+def linetokenize():
+    global nlist
+    instnlist = nlist
+    return instnlist
+
+def bow():
+    global instv
+    instb = instv
+    return instb
+
+def vector(getlist, tfidf = 0):
+    global instv
+    global nlist
+    klist = []
+    nlist = []
+    c = 0
+    varstring = " ".join(getlist)
+    instvect = Words(varstring)
+    instv = instvect.load()
+    for i in getlist:
+        insth = Words(i)
+        w = insth.load()
+        nlist.append(w)
+    for i in range(len(nlist)):
+            klist.append([0]*len(instv))
+    if tfidf == "tf":
+        for i in nlist:
+            for j in i:
+                c = instv.count(j)
+                klist[nlist.index(i)][instv.index(j)] = c / len(instv)
+
+    elif tfidf == "idf":
+        for i in nlist:
+            for j in i:
+                klist[nlist.index(i)][instv.index(j)] = math.log10(len(nlist)/sum([1.0 for i in nlist if j in i]))
+
+    elif tfidf == "tfidf":
+        for i in nlist:
+            for j in i:
+                c = instv.count(j)
+                vartf = c / len(instv)
+                varidf = math.log10(len(nlist)/sum([1.0 for i in nlist if j in i]))
+                klist[nlist.index(i)][instv.index(j)] = vartf * varidf
+
+    elif tfidf == 0:
+        for i in nlist:
+            for j in i:
+                klist[nlist.index(i)][instv.index(j)] = 1
+
+    return klist
