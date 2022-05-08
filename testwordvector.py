31ce8bb1e90376dcba9cf8dff522dbb5b07076ee
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-08 10:41:18 +0500

    Update testwordvector.py

--- testwordvector.py
+++ testwordvector.py
@@ -37,14 +37,6 @@ xdict["sentok"] = dlist
 # dump json
 json.dump(xdict, open("data.json", "w"))
 
-# load json
-d = json.load(open("data.json"))
-print("load json: \n", d)
-
-vect2 = d["vector"]
-vectbow2 = d["bow"]
-vectsent2 = d["sentok"]
-
 # tf-idf of words without 0
 count = 0
 for i in dlist:
@@ -57,23 +49,3 @@ listd1 = list(ydict.items())
 listd1.sort(key = lambda k: k[1])
 for i in listd1:
     print(i[0], "-", i[1])
-
-
-def main():
-    # search tf-idf of word
-    inp = input("word: ")
-    countw = 0
-    flag = 0
-    for i in vectsent2:
-        if flag == 0:
-            for j in i:
-                countw += 1
-                if j == inp:
-                    print("{}:{}".format(j, vect2[vectsent2.index(i)][countw -1]))
-                    flag = 1
-                    break
-        if countw == len(vectbow2):
-            print("the word does not exist in the documents")
-
-while True:
-    main()
