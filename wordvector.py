41581962d77b625ea6cb23e3c825c147c806a271
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-24 22:03:44 +0500

    Add files via upload

--- wordvector.py
+++ wordvector.py
@@ -14,37 +14,88 @@ class WordVector:
         for i in self.getlist:
             for j in i:
                 instv.append(j)
-        for i in range(len(self.getlist)):
-            klist.append([0]*len(instv))
+
         if self.tfidf == "tf":
             for i in self.getlist:
+                tlist = []
+                tlist2 = []
+                if countw != 0:
+                    tlist.append([0] * countw)
                 for j in i:
                     countw += 1
                     c = instv.count(j)
-                    klist[self.getlist.index(i)][countw - 1] = c / len(instv)
+                    tlist.append(c / len(instv))
+                tlist.append([0] * (len(instv) - countw))
+                for i in tlist:
+                    tlist2.extend(i)
+                klist.append(tlist2)
 
         elif self.tfidf == "idf":
             for i in self.getlist:
+                tlist = []
+                tlist2 = []
+                if countw != 0:
+                    tlist.append([0] * countw)
                 for j in i:
                     countw += 1
-                    klist[self.getlist.index(i)][countw -1] = math.log10(len(self.getlist)/sum([1.0 for i in self.getlist if j in i]))
+                    tlist.append(math.log10(len(self.getlist)/sum([1.0 for i in self.getlist if j in i])))
+                tlist.append([0] * (len(instv) - countw))
+                for i in tlist:
+                    tlist2.extend(i)
+                klist.append(tlist2)
 
         elif self.tfidf == "tf-idf":
             for i in self.getlist:
+                tlist = []
+                tlist2 = []
+                if countw != 0:
+                    tlist.append([0] * countw)
                 for j in i:
                     countw += 1
                     c = instv.count(j)
                     vartf = c / len(instv)
                     varidf = math.log10(len(self.getlist)/sum([1.0 for i in self.getlist if j in i]))
-                    klist[self.getlist.index(i)][countw -1] = vartf * varidf
+                    klist.append(vartf * varidf)
+                tlist.append([0] * (len(instv) - countw))
+                for i in tlist:
+                    tlist2.extend(i)
+                klist.append(tlist2)
 
         elif self.tfidf == "01":
             for i in self.getlist:
+                tlist = []
+                tlist2 = []
+                if countw != 0:
+                    tlist.append([0] * countw)
                 for j in i:
                     countw += 1
-                    klist[self.getlist.index(i)][countw - 1] = 1
+                    tlist.append([1])
+                tlist.append([0] * (len(instv) - countw))
+                for i in tlist:
+                    tlist2.extend(i)
+                klist.append(tlist2)
+
 
         elif self.tfidf == "custom":
+            for i in self.getlist:
+                tlist = []
+                tlist2 = []
+                if countw != 0:
+                    tlist.append([0] * countw)
+                for j in i:
+                    countw += 1
+                    c = 0
+                    for k in self.lcustomlist:
+                        c += 1
+                        if k == j:
+                            klist.append(self.rcustomlist[self.lcustomlist.index(k)])
+                            break
+                    if c == len(self.lcustomlist):
+                        klist.append(1)
+                tlist.append([0] * (len(instv) - countw))
+                for i in tlist:
+                    tlist2.extend(i)
+                klist.append(tlist2)
             for i in self.getlist:
                 for j in i:
                     countw += 1
@@ -55,6 +106,6 @@ class WordVector:
                             klist[self.getlist.index(i)][countw - 1] = self.rcustomlist[self.lcustomlist.index(k)]
                             break
                     if c == len(self.lcustomlist):
-                        klist[self.getlist.index(i)][countw - 1] = 1 
+                        klist[self.getlist.index(i)][countw - 1] = 1
         
         return klist
