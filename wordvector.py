a2bcc97a04fd748b273e6c69a2293dca1513c068
vbucode <61677060+vbucode@users.noreply.github.com>
2022-07-08 15:49:56 +0500

    Add files via upload

--- wordvector.py
+++ wordvector.py
@@ -45,6 +45,8 @@ class WordVector:
                 klist.append(tlist2)
 
         elif self.tfidf == "tf-idf":
+            memotf = {}
+            memoidf = {}
             for i in self.getlist:
                 tlist = []
                 tlist2 = []
@@ -52,10 +54,19 @@ class WordVector:
                     tlist.append([0] * countw)
                 for j in i:
                     countw += 1
-                    c = instv.count(j)
-                    vartf = c / len(instv)
-                    varidf = math.log10(len(self.getlist)/sum([1.0 for i in self.getlist if j in i]))
-                    tlist.append([vartf * varidf])
+                    if j not in memotf.keys():
+                        c = instv.count(j)
+                        vartf = c / len(instv)
+                        memotf[j] = vartf
+                    else:
+                        vartf = memotf[j]
+                    if j not in memoidf.keys():
+                        varidf = math.log10(len(self.getlist)/sum([1.0 for i in self.getlist if j in i]))
+                        tlist.append([vartf * varidf])
+                        memoidf[j] = varidf
+                    else:
+                        varidf = memoidf[j]
+                        tlist.append([vartf * varidf])
                 tlist.append([0] * (len(instv) - countw))
                 for k in tlist:
                     tlist2.extend(k)
